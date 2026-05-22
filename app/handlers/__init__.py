c596a1507af919041381a846e5a7b9c798dc0d34
fishselller <gluhota08@gmail.com>
2026-05-22 18:03:20 +0300

    w

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -6,6 +6,7 @@ def setup_routers() -> Router:
     from app.handlers.report import router as report_router
     from app.handlers.photo  import router as photo_router
     from app.handlers.help   import router as help_router
+    from app.handlers.stats  import router as stats_router
     from app.handlers.food   import router as food_router
 
     root = Router()
@@ -13,5 +14,6 @@ def setup_routers() -> Router:
     root.include_router(report_router)
     root.include_router(photo_router)
     root.include_router(help_router)
+    root.include_router(stats_router)
     root.include_router(food_router)
     return root
